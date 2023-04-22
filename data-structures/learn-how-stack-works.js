819d1ff016018d099d3444d0a30c6a5bfa5d58b8
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-04-22 07:58:33 -0300

    docs(data-structures): solution Learn How a Stack Works :memo:

---
+++ data-structures/learn-how-stack-works.js
@@ -0,0 +1,10 @@
+/*
+  Modify the given array and treat it like a stack using the JavaScript methods mentioned above.
+  Remove the top element "PSY44" from the stack. Then add "CS50"
+  to be the new top element of the stack.
+*/
+
+const homeworkStack = ["BIO12", "HIS80", "MAT122", "PSY44"];
+// Only change code below this line
+homeworkStack.pop();
+homeworkStack.push("CS50");
